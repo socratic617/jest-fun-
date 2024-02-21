2663926c7471f0a1b4477057d0b0597abeb51aef
socratic617 <erik.solis02135@gmail.com>
2024-02-21 18:56:54 -0500

    arrow function for efficiency

--- remainder.js
+++ remainder.js
@@ -1,5 +1,10 @@
-function findRemainder(x,y){
-  return x % y
-}
+//First test case
+// function findRemainder(x,y){
+//   return x % y
+// }
 
+//test function better
+
+//solution via arrow function
+const findRemainder = (x,y) => x % y
 module.exports = findRemainder
